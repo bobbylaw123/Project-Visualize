81e7a5ab04b936a8f07625acb2659b4ab88b53d6
bobbylaw123 <68624176+bobbylaw123@users.noreply.github.com>
2020-12-09 02:28:25 -0500

    init client commit w/ js
    init working client commit to pull a table from postgresSQL using node.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const Client = require('pg').Client
+const client = new Client({
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "covid_cases"
+})
+
+client.connect()
+.then(() => console.log("Connected Successfully"))
+.then(() => client.query("select * from march_22_2020"))
+.then(results => console.table(results.rows))
+.catch(e => console.log(e))
+.finally(() => client.end())
