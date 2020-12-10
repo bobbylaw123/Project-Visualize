b76fcaa4deaf3cba1ba4abddf586fc99770c9458
bobbylaw123 <68624176+bobbylaw123@users.noreply.github.com>
2020-12-10 14:04:49 -0500

    commit 2.02 pm
    now scrapes only the latest csv file from data source, cleans and sends to local postgresql database, runs automatically with flask in app.py with a nav bar for different things we want to show (index - dashboard, about - us and data source)

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-import scrape
-#from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import scrapeLatest
+from Postgres_Login import username, password, database
 
-scrape.scraper()
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+engine = create_engine(f"postgresql://{username}:{password}@localhost:5432/{database}")
+app.config['SQLALCHEMY_DATABASE_URI'] = engine
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    names = ["Jeff Chow", "Abhijit Purru", "Elise Eng"]
+    return render_template("about.html", names = names)
