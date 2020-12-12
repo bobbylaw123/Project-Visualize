535a3a070ae2e167f2f963cec1f9eda361698376
bobbylaw123 <68624176+bobbylaw123@users.noreply.github.com>
2020-12-12 02:59:32 -0500

    updates + drop descriptions in bar.js & app.py

--- static/js/bar.js
+++ static/js/bar.js
@@ -2,14 +2,19 @@ d3.json("/table_names").then((response) => {
 
   console.log(response)
 
+  // Drop down menu set up by appending every table_name into said menu.
   var dropdownMenu = document.getElementById("dropdown")
   for (let i = 0; i < response.length; i++){
       let newOption = document.createElement("option")
       newOption.innerHTML = response[i].table_name
           dropdownMenu.appendChild(newOption)
       }
-      
+
+  // Response to a requested date that was selected from drop down menu.
+  // Sent to /bar to form a json str.
   dropdownMenu.addEventListener("change", event => {
+
+      // Reading the json str with new data to be made into a bar chart. 
       d3.json(`/bar?requested_date=${event.target.value}`).then(response => {
           console.log(response);
 
@@ -45,30 +50,30 @@ d3.json("/table_names").then((response) => {
           var data = [trace1, trace2, trace3];
         
           var layout = {
-            title: 'Covid Cases on Active Cases, Deaths and Recoveries by Day',
+            title: 'Covid Cases on Active Cases, Deaths and Recoveries on Requested Day',
             titlefont: {
-              size:40,
+              size:30,
               color: 'rgb(107, 107, 107)'
             },
             xaxis: {
               title: 'US States',
               titlefont: {
-                size:30,
+                size:23,
                 color: 'rgb(107, 107, 107)'
               },
               tickfont: {
-                size: 13,
+                size: 10,
                 color: 'rgb(107, 107, 107)'
               }
             },
             yaxis: {
               title: 'Cases by Active, Deaths and Recoveries',
               titlefont: {
-                size: 30,
+                size: 20,
                 color: 'rgb(107, 107, 107)'
               },
               tickfont: {
-                size: 15,
+                size: 13,
                 color: 'rgb(107, 107, 107)'
               }
             },
