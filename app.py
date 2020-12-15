4d6ed18ff43bc4afce885e97e45f2a2b0c51b0d2
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2020-12-15 05:05:20 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,127 @@
-from flask import Flask
+import json
+#import scrape
+#import scrapeLatest
+import psycopg2
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-import scrape
-from loginInfo import username, password
+from Postgres_Login import username, password, database
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-engine = create_engine(f"postgresql://{username}:{password}@localhost:5432/covid_cases")
+engine = create_engine(f"postgresql://{username}:{password}@localhost:5432/{database}")
 app.config['SQLALCHEMY_DATABASE_URI'] = engine
 db = SQLAlchemy(app)
 
 @app.route('/')
-def test():
-    dfList = scrape.scraper()
-    for data in dfList:
-        dfList[data].to_sql(name=data, con=engine, if_exists="append", index=False)
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
+@app.route('/about')
+def about():
+    names = ["Jeff Chow", "Abhijit Purru", "Elise Eng"]
+    return render_template("about.html", names = names)
 
-if __name__ == '__main__':
-    app.run()
+# All the names of the tables to be listed in the drop down menu.
+@app.route('/table_names')
+def table_names():
+    name = engine.execute("""   SELECT table_name FROM information_schema.tables
+                                WHERE table_schema='public'""").fetchall()
+
+    print (name)
+    return json.dumps([dict(r) for r in name])
+
+# Pulls data from the requested date on the drop down menu to json string.
+@app.route('/bar')
+def bar():
+    
+    # The drop down menu's requested date input
+    requested_date = request.args.get('requested_date')
+
+    results = engine.execute(f"""   select state, sum(deaths), sum(recovered), sum(active)
+                                    from {requested_date} group by state
+                                    order by state""").fetchall()
+
+    states_list = []
+    deaths_list = []
+    recovered_list = []
+    active_list = []
+
+    for state, deaths, recovered, active in results:
+        states_list.append(state)
+        deaths_list.append(int(deaths))
+        recovered_list.append(int(recovered))
+        active_list.append(int(active))
+
+    results_dic = {"state": states_list, "deaths": deaths_list, "recovered": recovered_list, "active": active_list}
+
+    print(results_dic)
+    return jsonify(results_dic)
+
+@app.route('/map')
+
+def map():
+
+    # The drop down menu's requested date input
+    requested_date = request.args.get('requested_date')
+
+    results = engine.execute(f"""   select location, latitude, longitude, sum(deaths), sum(recovered), sum(active)
+                                    from {requested_date} group by location, latitude, longitude
+                                    order by location""").fetchall()
+
+    location_list = []
+    coordinates_list = []
+    deaths_list = []
+    recovered_list = []
+    active_list = []
+
+    for location, latitude, longitude, deaths, recovered, active in results:
+        location_list.append(location)
+        coordinates_list.append([latitude,longitude])
+        deaths_list.append(int(deaths))
+        recovered_list.append(int(recovered))
+        active_list.append(int(active))
+
+    results_dic = {"location": location_list, "coordinates": coordinates_list,
+                   "deaths": deaths_list, "recovered": recovered_list, "active": active_list}
+
+    print(results_dic)
+    return jsonify(results_dic)
+
+@app.route('/maps')
+def maps():
+    return render_template("map.html")
+
+# Sending info from jsonify data pulled from previous route into bar.html to form bar chart.
+@app.route('/bars')
+def bars():
+    return render_template("bar.html")
+
+@app.route('/piepg')
+def piepg():
+    return render_template("pie.html")
+
+@app.route('/pie')
+def pie():
+    results = engine.execute("""select state, sum(deaths), sum(recovered), sum(active)
+                                from latest_data group by state
+                                order by state""").fetchall()
+
+    states_list = []
+    deaths_list = []
+    recovered_list = []
+    active_list = []
+    for state, deaths, recovered, active in results:
+        states_list.append(state)
+        deaths_list.append(int(deaths))
+        recovered_list.append(int(recovered))
+        active_list.append(int(active))
+
+    results_dic = {"state": states_list, "deaths": deaths_list,
+                   "recovered": recovered_list, "active": active_list}
+
+    print(results_dic)
+    return jsonify(results_dic)
+
+if __name__ == "__main__":
+    app.run(debug=True)
