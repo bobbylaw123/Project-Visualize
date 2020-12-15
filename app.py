d34499ed836ef3710681d3bdd65b5f0822d28f46
lotuselise3 <68654746+lotuselise3@users.noreply.github.com>
2020-12-15 11:24:30 -0800

    add line route to flask

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-=======
 import json
 # import scrape
 # import scrapeLatest
 import psycopg2
->>>>>>> 15340126de60ad2e0527b2584cf6abddcdf8f1b0
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from Postgres_Login import username, password, database
 
-<<<<<<< HEAD
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-engine = create_engine(
-    f"postgresql://{username}:{password}@localhost:5432/{database}")
-app.config['SQLALCHEMY_DATABASE_URI'] = engine
-db = SQLAlchemy(app)
-
-
-=======
 # Flask startup
 app = Flask(__name__)
 
@@ -29,36 +16,10 @@ engine = create_engine(f"postgresql://{username}:{password}@localhost:5432/{data
 app.config['SQLALCHEMY_DATABASE_URI'] = engine
 db = SQLAlchemy(app)
 
->>>>>>> 15340126de60ad2e0527b2584cf6abddcdf8f1b0
 @app.route('/')
 def index():
     return render_template("index.html")
 
-<<<<<<< HEAD
-
-@app.route('/linepg')
-def linepg():
-    return render_template("line.html")
-
-
-@app.route('/bars')
-def bars():
-    return render_template("bar.html")
-
-
-@app.route('/about')
-def about():
-    names = ["Jeff Chow", "Abhijit Purru", "Elise Eng"]
-    return render_template("about.html", names=names)
-
-
-@app.route('/line')
-def line():
-    results = engine.execute(f"""   select state, sum(deaths), sum(confirmed)
-                                from latest_data group by state
-                                order by state""").fetchall()
-
-=======
 @app.route('/about')
 def about():
     names = ["Jeff Chow", "Abhijit Purru", "Elise Eng"]
@@ -82,46 +43,10 @@ def bar():
     results = engine.execute(f"""   select state, sum(deaths), sum(confirmed)
                                     from {requested_date} group by state
                                     order by state""").fetchall()
->>>>>>> 15340126de60ad2e0527b2584cf6abddcdf8f1b0
     states_list = []
     deaths_list = []
     recovered_list = []
     confirmed_list = []
-<<<<<<< HEAD
-for state, deaths, confirmed in results:
-    recovered = (int(confirmed)-int(deaths))
-    states_list.append(state)
-    deaths_list.append(int(deaths))
-    recovered_list.append(int(recovered))
-    confirmed_list.append(int(confirmed))
-    results_dic = {"state": states_list, "deaths": deaths_list,
-               "recovered": recovered_list, "confirmed": confirmed_list}
-    print(results_dic)
-    return jsonify(results_dic)
-
-
-@app.route('/bar')
-def bar():
-    results = engine.execute("""select state, sum(deaths), sum(recovered), sum(active)
-from december_09_2020 group by state
-order by state""").fetchall()
-
-    # results = [(state, deaths, recovered, active), (state, deaths, recovered, active)...]
-    # results = {"state": ["Alabama", "Arizona", ...], "deaths": [deaths1, deaths2,...], }
-
-    states_list = []
-    deaths_list = []
-    recovered_list = []
-    active_list = []
-    for state, deaths, recovered, active in results:
-        states_list.append(state)
-        deaths_list.append(int(deaths))
-        recovered_list.append(int(recovered))
-        active_list.append(int(active))
-
-    results_dic = {"state": states_list, "deaths": deaths_list,
-                   "recovered": recovered_list, "active": active_list}
-=======
 
     for state, deaths, confirmed in results:
 
@@ -134,21 +59,40 @@ order by state""").fetchall()
 
     results_dic = {"state": states_list, "deaths": deaths_list,
                     "recovered": recovered_list, "confirmed": confirmed_list}
->>>>>>> 15340126de60ad2e0527b2584cf6abddcdf8f1b0
 
     print(results_dic)
     return jsonify(results_dic)
 
-<<<<<<< HEAD
-
-if __name__ == "__main__":
-    app.run(debug=True)
-=======
 # Sending info from jsonify data pulled from previous route into bar.html to form bar chart.
 @app.route('/bars')
 def bars():
     return render_template("bar.html")
 
+@app.route('/line')
+def line():
+    # change december_09_2020 to latest_data in production
+    results = engine.execute(f"""select state, sum(deaths), sum(confirmed)
+    from december_09_2020 group by state
+    order by state""").fetchall()
+
+    states_list = []
+    deaths_list = []
+    recovered_list = []
+    confirmed_list = []
+    for state, deaths, confirmed in results:
+        recovered = (int(confirmed)-int(deaths))
+        states_list.append(state)
+        deaths_list.append(int(deaths))
+        recovered_list.append(int(recovered))
+        confirmed_list.append(int(confirmed))
+    results_dic = {"state": states_list, "deaths": deaths_list,
+               "recovered": recovered_list, "confirmed": confirmed_list}
+    print(results_dic)
+    return jsonify(results_dic)
+
+@app.route('/linepg')
+def linepg():
+    return render_template("line.html")
+        
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> 15340126de60ad2e0527b2584cf6abddcdf8f1b0
