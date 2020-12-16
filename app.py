65c9b6d6fd8168604c0ea922865b54107ba2ffdd
lotuselise3 <68654746+lotuselise3@users.noreply.github.com>
2020-12-15 23:42:50 -0800

    Add files via upload
    I give up. sorry for letting you guys down. You can play around with what I've started, but my brain won't work anymore.

--- app.py
+++ app.py
@@ -1,98 +1,84 @@
-import json
-# import scrape
-# import scrapeLatest
-import psycopg2
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from Postgres_Login import username, password, database
 
-# Flask startup
 app = Flask(__name__)
-
-# PostgreSQL connect
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-engine = create_engine(f"postgresql://{username}:{password}@localhost:5432/{database}")
+engine = create_engine(
+    f"postgresql://{username}:{password}@localhost:5432/{database}")
 app.config['SQLALCHEMY_DATABASE_URI'] = engine
 db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    names = ["Jeff Chow", "Abhijit Purru", "Elise Eng"]
-    return render_template("about.html", names = names)
 
-# All the names of the tables to be listed in the drop down menu.
-@app.route('/table_names')
-def table_names():
-    name = engine.execute("""   SELECT table_name FROM information_schema.tables
-                                WHERE table_schema='public'""").fetchall()
+@app.route('/linepg')
+def linepg():
+    return render_template("line.html")
 
-    print (name)
-    return json.dumps([dict(r) for r in name])
 
-# Pulls data from the requested date on the drop down menu to json string.
-@app.route('/bar')
-def bar():
-    
-    # The drop down menu's requested date input
-    requested_date = request.args.get('requested_date')
-    results = engine.execute(f"""   select state, sum(deaths), sum(confirmed)
-                                    from {requested_date} group by state
-                                    order by state""").fetchall()
-    states_list = []
-    deaths_list = []
-    recovered_list = []
-    confirmed_list = []
-
-    for state, deaths, confirmed in results:
-
-        recovered = (int(confirmed)-int(deaths))
-        
-        states_list.append(state)
-        deaths_list.append(int(deaths))
-        recovered_list.append(int(recovered))
-        confirmed_list.append(int(confirmed))
-
-    results_dic = {"state": states_list, "deaths": deaths_list,
-                    "recovered": recovered_list, "confirmed": confirmed_list}
-
-    print(results_dic)
-    return jsonify(results_dic)
-
-# Sending info from jsonify data pulled from previous route into bar.html to form bar chart.
 @app.route('/bars')
 def bars():
     return render_template("bar.html")
 
-@app.route('/hist')
-def hist():
-    # change december_09_2020 to latest_data in production
-    results = engine.execute(f"""select state, sum(deaths), sum(confirmed)
-    from latest_data group by state
-    order by state""").fetchall()
+
+@app.route('/about')
+def about():
+    names = ["Jeff Chow", "Abhijit Purru", "Elise Eng"]
+    return render_template("about.html", names=names)
+
+
+@app.route('/line')
+def line():
+    results = engine.execute(f"""   select state, sum(deaths), sum(confirmed)
+                                from latest_data group by state
+                                order by state""").fetchall()
 
     states_list = []
     deaths_list = []
     recovered_list = []
     confirmed_list = []
-    for state, deaths, confirmed in results:
-        recovered = (int(confirmed)-int(deaths))
-        states_list.append(state)
-        deaths_list.append(int(deaths))
-        recovered_list.append(int(recovered))
-        confirmed_list.append(int(confirmed))
+for state, deaths, confirmed in results:
+    recovered = (int(confirmed)-int(deaths))
+    states_list.append(state)
+    deaths_list.append(int(deaths))
+    recovered_list.append(int(recovered))
+    confirmed_list.append(int(confirmed))
     results_dic = {"state": states_list, "deaths": deaths_list,
                "recovered": recovered_list, "confirmed": confirmed_list}
     print(results_dic)
     return jsonify(results_dic)
 
-@app.route('/histpg')
-def histpg():
-    return render_template("hist.html")
+
+@app.route('/bar')
+def bar():
+    results = engine.execute("""select state, sum(deaths), sum(recovered), sum(active)
+from december_09_2020 group by state
+order by state""").fetchall()
+
+    # results = [(state, deaths, recovered, active), (state, deaths, recovered, active)...]
+    # results = {"state": ["Alabama", "Arizona", ...], "deaths": [deaths1, deaths2,...], }
+
+    states_list = []
+    deaths_list = []
+    recovered_list = []
+    active_list = []
+    for state, deaths, recovered, active in results:
+        states_list.append(state)
+        deaths_list.append(int(deaths))
+        recovered_list.append(int(recovered))
+        active_list.append(int(active))
+
+    results_dic = {"state": states_list, "deaths": deaths_list,
+                   "recovered": recovered_list, "active": active_list}
+
+    print(results_dic)
+    return jsonify(results_dic)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- hist.js
+++ hist.js
@@ -1,54 +1,138 @@
+var chart, data;
+
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#hist")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+var color = "steelblue";
+
+// get the data
 d3.json("/hist").then((response) => {
 
   console.log(response);
 
-  var deaths = response.deaths;
-  var recovered = response.recovered;
-  var confirmed = response.confirmed;
+  // title: 'Histogram of COVID-19 US Daily Cases',
+var x = d3.scaleLinear()
+  .domain([0, 1.7])
+  .range([0, width]);
+svg.append("states")             
+  .attr("x", 480 )
+  .attr("y",  475 )
+  .style("text-anchor", "middle")
+  .text("Date");
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
 
-  console.log(deaths);
-  console.log(recovered);
-  console.log(confirmed);
-  
-  var trace1 = {
-    x: confirmed,
-    type: "histogram",
-    opacity: 0.5,
-    name: "Confirmed",
-    marker: {
-       color: 'green',
-    },
-  };
-  var trace2 = {
-    x: deaths,
-    type: "histogram",
-    opacity: 0.8,
-    name: "Deaths",
-    marker: {
-       color: 'red',
-    },
-  };
-  
-  var trace3 = {
-    x: recovered,
-    type: "histogram",
-    opacity: 0.6,
-    name: "Recovered",
-    marker: {
-       color: 'blue',
-    },
-  };
+  // Generate a histogram using twenty uniformly-spaced bins.
+  var data = d3.layout.histogram()
+    .bins(x.ticks(20))
+    (values);
 
-  var data = [trace2, trace3, trace1];
+  var yMax = d3.max(data, function (d) { return d.length });
+  var yMin = d3.min(data, function (d) { return d.length });
+  var colorScale = d3.scale.linear()
+    .domain([yMin, yMax])
+    .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
 
-  var layout = {
-    title: 'Histogram of COVID-19 US Daily Cases',
-    xaxis: {title: "Value"}, 
-    yaxis: {title: "Cases Count"},
-    height: 800,
-    width: 900,
-    barmode: "stack"
-  };
-  
-  Plotly.newPlot('hist', data, layout);
-});
+  var y = d3.scaleLinear()
+    .domain([0, yMax])
+    .range([height, 0]); 
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+  var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var bar = svg.selectAll(".bar")
+    .data(data)
+    .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function (d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+  bar.append("rect")
+    .attr("x", 1)
+    .attr("width", (x(data[0].dx) - x(0)) - 1)
+    .attr("height", function (d) { return height - y(d.y); })
+    .attr("fill", function (d) { return colorScale(d.y) });
+
+  bar.append("text")
+    .attr("dy", ".75em")
+    .attr("y", -12)
+    .attr("x", (x(data[0].dx) - x(0)) / 2)
+    .attr("text-anchor", "middle")
+    .text(function (d) { return formatCount(d.y); });
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  /*
+  * Adding refresh method to reload new data
+  */
+  function refresh(values) {
+    // var values = d3.range(1000).map(d3.random.normal(20, 5));
+    var data = d3.layout.histogram()
+      .bins(x.ticks(20))
+      (values);
+
+    // Reset y domain using new data
+    var yMax = d3.max(data, function (d) { return d.length });
+    var yMin = d3.min(data, function (d) { return d.length });
+    y.domain([0, yMax]);
+    var colorScale = d3.scale.linear()
+      .domain([yMin, yMax])
+      .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
+
+    var bar = svg.selectAll(".bar").data(data);
+
+    // Remove object with data
+    bar.exit().remove();
+
+    bar.transition()
+      .duration(1000)
+      .attr("transform", function (d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+    bar.select("deaths")
+      .transition()
+      .duration(1000)
+      .attr("height", function (d) { return height - y(d.y); })
+      .attr("fill", function (d) { return colorScale(d.y) });
+
+    bar.select("recovered")
+      .transition()
+      .duration(1000)
+      .text(function (d) { return formatCount(d.y); });
+
+    bar.select("confirmed")
+      .transition()
+      .duration(1000)
+      .text(function (d) { return formatCount(d.y); });
+
+  }
+
+  // Calling refresh repeatedly.
+  setInterval(function () {
+    var values = d3.range(1000).map(d3.random.normal(20, 5));
+    refresh(values);
+  }, 2000);
+
+  // title: "Total Number of Cases, Deaths and Affected",
+
+  });
